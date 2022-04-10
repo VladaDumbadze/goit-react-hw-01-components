2e0c4d23277b698d83b234e99f4e0ffc3344ccb6
VladaDumbadze <87700517+VladaDumbadze@users.noreply.github.com>
2022-04-10 15:56:32 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,5 @@
+import { FriendList } from './friend-list/FriendList';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <FriendList />;
 };
---
+++ src/components/friend-list/FriendList.jsx
@@ -0,0 +1,5 @@
+import css from './FriendList.module.css';
+
+export const FriendList = () => {
+  return <ul className={css.FriendList}> </ul>;
+};
